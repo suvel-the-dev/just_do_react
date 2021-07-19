04a77f48319b168769acbf22b77a03dfdd2cfc68
Suvel <suvelrathneswar08@gmail.com>
2021-07-19 09:55:04 +0530

    added counter and increment button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCounter = () => {
+    setCount(previousCount => previousCount + 1);
+  }
+
   return (
     <div className="App">
-      hello world
+      <div
+        className='counter'
+      >
+        {count}
+      </div>
+      <button
+        className='counter__increment'
+        onClick={incrementCounter}
+      >
+        Click 👆
+      </button>
     </div>
   );
 }
